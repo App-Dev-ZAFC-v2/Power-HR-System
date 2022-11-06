e8d9a7f86ae600ef15fda711e69386bc561ae34b
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-11-06 16:24:12 +0800

    HRS-63 Fix Files

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import ApplicantRegister from './Pages/ApplicantRegister';
+import EmployeeDashboard from './Pages/Employee/Dashboard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<ApplicantRegister />} />
+        <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
       </Routes>
     </div>
   );
