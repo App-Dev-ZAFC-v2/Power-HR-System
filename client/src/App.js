bea8cc47b64f4474003ed4f0e24f97cc7371ad89
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-11-06 16:14:18 +0800

    HRS-63 Updated Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
-import ApplicantRegister from './Pages/ApplicantRegister';
+import ApplicantRegister from './Pages/Applicant/Register';
+import EmployeeDashboard from './Pages/Employee/Dashboard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<ApplicantRegister />} />
+        <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
       </Routes>
     </div>
   );
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import {Form, Button, Container} from 'react-bootstrap';
 
 function Login() {
-  return (
-    <div>login (commit) hmmm</div>
-  )
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [userType, setUserType] = useState(['applicants', 'employees', 'admins']);
+    const [user, setUser] = useState('');
+    const [error, setError] = useState('');
+    const [tries, setTries] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const loginData = {
+            username,
+            password
+        }
+        axios.post(`http://localhost:5000/${userType[user]}/login`, loginData)
+        .then(res => {
+            console.log(res);
+            localStorage.setItem('authToken', res.data.token);
+            user===0 ? window.location = '/applicant' : 
+            user===1 ? window.location = '/employee' : 
+            window.location = '/admin/dashboard';
+        
+        }
+        )
+        .catch(err => {
+            setError(err.response.data.error);
+            setTries(tries + 1);
+        }
+        )
+    }
+
+    return (
+        <>
+        <Container>
+        <div className='uni-form'>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group p-3">
+                <label htmlFor="username">Username</label>
+                <input type="text" className="form-control" id="username" placeholder="Enter username" value={username} required onChange={(e) => setUsername(e.target.value)} autoComplete="off" />
+            </div>
+            <div className="form-group p-3">
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} required onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit" className="btn btn-primary">Login</button>
+        </form>
+        {error && <div className="alert alert-danger">{error}</div>}
+        </div>
+    </Container>
+        </>
+    )
 }
 
 export default Login
