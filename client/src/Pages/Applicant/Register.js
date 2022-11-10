ae7d308c71f259fde872c01a4e90686c2e9b49f1
Brendan Dylan <73895780+rhodylan@users.noreply.github.com>
2022-11-10 20:09:12 +0800

    HRS-65
    visible validation of user input (visual added)

--- client/src/Pages/Applicant/Register.js
+++ client/src/Pages/Applicant/Register.js
@@ -1,25 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Logo from "../../Assets/Logo.png";
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import axios from 'axios';
 
-// import '../../styles/Register.css';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import { PasswordSharp } from '@mui/icons-material';
+
+//validation of password and email
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function ApplicantRegister() {
-  const theme = createTheme();
 
+  const theme = createTheme();
+  
   const [user, setUser] = useState({
     applicantName: '',
     username: '',
@@ -28,20 +34,29 @@ function ApplicantRegister() {
     confirmPassword: '',
     applicantContact: '',
   });
-
+  
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState('');
 
+  //validate inputs
+  const validate = () =>{
+      let temp = {}
+      temp.applicantName = user.applicantName?"":"This field is required."
+      temp.username = user.username?"":"This field is required."
+      temp.password = PWD_REGEX.test(user.password)?"":"Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number."
+      temp.applicantEmail = EMAIL_REGEX.test(user.applicantEmail)?"":"Email is not valid."
+      temp.confirmPassword = user.confirmPassword === user.password ? "" : "Passwords do not match"
+      temp.applicantContact = user.applicantContact.length === 10 ? "" : "Contact number must be 10 digits"
+
+      setError({
+          ...temp
+      })
+
+      return Object.values(temp).every(x => x === "")
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const registerData = {
-    //   username,
-    //   password,
-    //   confirmPassword,
-    //   applicantName,
-    //   applicantEmail,
-    //   applicantContact
-    // };
     axios
       .post(`http://localhost:5000/applicants/register`, user)
       .then((res) => {
@@ -51,8 +66,9 @@ function ApplicantRegister() {
         window.location.href = '/login';
       })
       .catch((err) => {
-        console.log(err);
-        setError(err.response.data.message);
+        // console.log(err);
+        // setError({ ...error, fetchError: true, fetchErrorMsg: err.response.data.message });
+        validate();
       });
   };
 
@@ -70,18 +86,18 @@ function ApplicantRegister() {
           border: '1px solid black',
           borderRadius: '10px',
           padding: '20px',
-          marginTop: '20vh',
+          marginTop: '10vh',
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
+        <Avatar sx={{ m: 1, bgcolor: 'grey.900' }} alt="Your logo."
+          src={Logo} />
+        <Typography sx={{mb: 2}} variant="body">Power HR System</Typography>
         <Typography component="h1" variant="h5">
           Let's get started!
         </Typography>
         <Box component="form" noValidate onSubmit={(e) => handleSubmit(e)} sx={{ mt: 3, }}>
           <Grid container spacing={2}>
-          <Grid item xs={12}>
+            <Grid item xs={12}>
               <TextField
                 autoComplete="fname"
                 name="applicantName"
@@ -92,6 +108,7 @@ function ApplicantRegister() {
                 value={user.applicantName}
                 onChange={(e) => setUser({ ...user, applicantName: e.target.value })}
                 autoFocus
+                {...(error.applicantName && {error:true, helperText:error.applicantName})}
               />
             </Grid>
             <Grid item xs={12}>
@@ -105,6 +122,7 @@ function ApplicantRegister() {
                 value={user.username}
                 onChange={(e) => setUser({ ...user, username: e.target.value })}
                 autoFocus
+                {...(error.username && {error:true, helperText:error.username})}
               />
             </Grid>
             <Grid item xs={12}>
@@ -117,6 +135,7 @@ function ApplicantRegister() {
                 autoComplete="applicantEmail"
                 value={user.applicantEmail}
                 onChange={(e) => setUser({ ...user, applicantEmail: e.target.value })}
+                {...(error.applicantEmail && {error:true, helperText:error.applicantEmail})}
               />
             </Grid>
             <Grid item xs={12}>
@@ -130,6 +149,7 @@ function ApplicantRegister() {
                 autoComplete="password"
                 value={user.password}
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
+                {...(error.password && {error:true, helperText:error.password})}
               />
             </Grid>
             <Grid item xs={12}>
@@ -143,6 +163,7 @@ function ApplicantRegister() {
                 autoComplete="confirmPassword"
                 value={user.confirmPassword}
                 onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })}
+                {...(error.confirmPassword && {error:true, helperText:error.confirmPassword})}
               />
             </Grid>
             <Grid item xs={12}>
@@ -155,6 +176,7 @@ function ApplicantRegister() {
                 autoComplete="applicantContact"
                 value={user.applicantContact}
                 onChange={(e) => setUser({ ...user, applicantContact: e.target.value })}
+                {...(error.applicantContact && {error:true, helperText:error.applicantContact})}
               />
             </Grid>
             {/* <Grid item xs={12}>
