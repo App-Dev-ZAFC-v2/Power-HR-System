ad975fcb9e192d3642ac031ea7dfd8c87e608798
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-11-11 23:09:52 +0800

    HRS-82 Added API call to get data user logged in

--- client/src/Components/ProfileCard.jsx
+++ client/src/Components/ProfileCard.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -8,6 +9,26 @@ import { Grid } from '@mui/material';
 import bg from '../Assets/BackgroundProfile/Cloudy.png'
 
 export default function ProfileCard() {
+
+    const [user, setUser] = useState([]);
+    const token = localStorage.getItem('authToken');
+    const userType = JSON.parse(atob(token.split('.')[1])).userType;
+    const userId = JSON.parse(atob(token.split('.')[1])).UserId;
+
+    useEffect(() => {
+        userType === 0 ? axios.get(`http://localhost:5000/applicants/${userId}`, {headers: {Authorization: `Bearer ${token}`}}) :
+        userType === 1 ? axios.get(`http://localhost:5000/admins/${userId}`, {headers: {Authorization: `Bearer ${token}`}}) :
+        axios.get(`http://localhost:5000/employees/${userId}`, {headers: {Authorization: `Bearer ${token}`}})
+        .then(res => {
+            setUser(res.data);
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+
   return (
     <Card variant="outlined" sx={{borderRadius: 3, maxHeight:400}}>
       <CardMedia
