dbd7446140c93f9c2137ce5dad476d986237b734
haziqzikry <haziqzikry@gmail.com>
2022-11-13 12:53:15 +0800

    HRS-24 Updated Feedback Model

--- server/models/feedback.js
+++ server/models/feedback.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const feedbackSchema = mongoose.Schema({
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employee",
+    },
     feedbackTitle: String,
     feedbackMessage: String,
+
 });
 
 const Feedback = mongoose.model("Feedback", feedbackSchema);
