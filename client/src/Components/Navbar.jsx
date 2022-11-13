b35b40d4230cfeb791a73a8e63659664b4feab2e
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2022-11-13 12:32:48 +0800

    HRS-50 Modal Popup for update

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,53 +1,58 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import Link from '@mui/material/Link';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import Link from "@mui/material/Link";
 import Logo from "../Assets/Logo.png";
 
-const settings = [{link:'/profile', name:'Profile'}, {link:'/login', name:'Logout'}];
+const settings = [
+  { link: "/applicant/profile", name: "Profile" },
+  { link: "/login", name: "Logout" },
+];
 
-function GetPages(){
+function GetPages() {
   const [user, setUser] = useState([]);
-  const token = localStorage.getItem('authToken');
-  
+  const token = localStorage.getItem("authToken");
+
   // FIND BETTER WAY TO DO THIS
-  const userType = JSON.parse(atob(token.split('.')[1])).userType;
-  const detailId = JSON.parse(atob(token.split('.')[1])).detailId;
+  const userType = JSON.parse(atob(token.split(".")[1])).userType;
+  const detailId = JSON.parse(atob(token.split(".")[1])).detailId;
 
   useEffect(() => {
-    (userType === 0 ? axios.get(`http://localhost:5000/applicants/${detailId}/`) :
-      axios.get(`http://localhost:5000/employees/${detailId}/`))
-      .then(res => {
-          setUser(res.data);
+    (userType === 0
+      ? axios.get(`http://localhost:5000/applicants/${detailId}/`)
+      : axios.get(`http://localhost:5000/employees/${detailId}/`)
+    )
+      .then((res) => {
+        setUser(res.data);
       })
-      .catch(err => {
-          console.log(err);
-      })
-  })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-
-  switch(userType){
-      case 0://Applicant
-        return [{link:'/nav1', name:'STATUS'}];
-      case 2://Employee & executive
-        if(user.executiveRole === true){
-          return [{link:'/nav5', name:'ANALYZE'}, {link:'/nav6', name:'FEEDBACK REVIEW'}]
-        }
-        else
-          return [{link:'/feedback', name:'FEEDBACK'}];
-      default:
-        return;
+  switch (userType) {
+    case 0: //Applicant
+      return [{ link: "/nav1", name: "STATUS" }];
+    case 2: //Employee & executive
+      if (user.executiveRole === true) {
+        return [
+          { link: "/nav5", name: "ANALYZE" },
+          { link: "/nav6", name: "FEEDBACK REVIEW" },
+        ];
+      } else return [{ link: "/feedback", name: "FEEDBACK" }];
+    default:
+      return;
   }
 }
 
@@ -71,13 +76,15 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static" sx={{backgroundColor:'#121212'}}>
+    <AppBar position="static" sx={{ backgroundColor: "#121212" }}>
       <Container maxWidth="lg">
         <Toolbar disableGutters>
           <Box
             component="img"
             sx={{
-                display: { xs: 'none', md: 'flex' }, mr: 1, height: 32
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+              height: 32,
             }}
             alt="Your logo."
             src={Logo}
@@ -89,18 +96,18 @@ function ResponsiveAppBar() {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             PowerHR
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -115,31 +122,35 @@ function ResponsiveAppBar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {GetPages().map((page) => (
-                <Link href={page.link} underline="none"><MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page.name}</Typography>
-                </MenuItem></Link>
+                <Link href={page.link} underline="none">
+                  <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </MenuItem>
+                </Link>
               ))}
             </Menu>
           </Box>
           <Box
             component="img"
             sx={{
-                display: { xs: 'flex', md: 'none' }, mr: 1, height: 32
+              display: { xs: "flex", md: "none" },
+              mr: 1,
+              height: 32,
             }}
             alt="Your logo."
             src={Logo}
@@ -151,26 +162,34 @@ function ResponsiveAppBar() {
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             PowerHR
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex',} }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {GetPages().map((page) => (
-              <Link href={page.link} underline="none"><Button
-                key={page.name}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block', background: "#121212", '&:hover': {background: "#f00"} }}
-              >
-                {page.name}
-              </Button></Link>
+              <Link href={page.link} underline="none">
+                <Button
+                  key={page.name}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    my: 2,
+                    color: "white",
+                    display: "block",
+                    background: "#121212",
+                    "&:hover": { background: "#f00" },
+                  }}
+                >
+                  {page.name}
+                </Button>
+              </Link>
             ))}
           </Box>
 
@@ -181,25 +200,27 @@ function ResponsiveAppBar() {
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {(settings).map((setting) => (
-                <Link href={setting.link} underline="none"><MenuItem key={setting.name} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting.name}</Typography>
-                </MenuItem></Link>
+              {settings.map((setting) => (
+                <Link href={setting.link} underline="none">
+                  <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting.name}</Typography>
+                  </MenuItem>
+                </Link>
               ))}
             </Menu>
           </Box>
--- client/src/Pages/Applicant/Profile.js
+++ client/src/Pages/Applicant/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../Components/Navbar";
 import {
   Grid,
@@ -8,12 +8,48 @@ import {
   CardActions,
   CardContent,
   Button,
+  Modal,
 } from "@mui/material";
 import ProfileCard from "../../Components/ProfileCard";
-// import ProfileCard from "../../Components/ProfileCard";
-// import UpdateForm from "./components/updateform";
+import UpdateForm from "./components/updateform";
+import axios from "axios";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+};
 
 function profile() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [user, setUser] = useState([]);
+  const token = localStorage.getItem("authToken");
+
+  // FIND BETTER WAY TO DO THIS
+  const userType = JSON.parse(atob(token.split(".")[1])).userType;
+  // const userId = JSON.parse(atob(token.split('.')[1])).UserId;
+  const detailId = JSON.parse(atob(token.split(".")[1])).detailId;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    (userType === 0
+      ? axios.get(`http://localhost:5000/applicants/${detailId}/`)
+      : axios.get(`http://localhost:5000/employees/${detailId}/`)
+    )
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -32,19 +68,32 @@ function profile() {
             </Typography>
             <Typography sx={{ mb: 1.5 }} color="text.secondary" mt={5}>
               <Typography variant="body">
-                Name: Muhammad Aniq Aqil
+                Name: {user?.name}
                 <br />
-                Username: aniq
+                Username: {user?.username}
                 <br />
-                Email:aniq@gmail.com
+                Email:{user?.email}
                 <br />
-                Phone Number: 0123456789
+                Phone Number: {user?.contact}
                 <br />
+                Position:{user?.position}
               </Typography>
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Update Profile</Button>
+            <Button size="small" onClick={handleOpen}>
+              Update Profile
+            </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <UpdateForm />
+              </Box>
+            </Modal>
           </CardActions>
         </Card>
       </Grid>
