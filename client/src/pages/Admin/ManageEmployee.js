15020e6e53cb3c6ac10183882c2c1b02a4216d5d
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-11-18 09:33:00 +0800

    HRS-73 Added table of employees

--- client/src/pages/Admin/ManageEmployee.js
+++ client/src/pages/Admin/ManageEmployee.js
@@ -1,13 +1,72 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Navbar from '../../Components/Navbar';
+import { Button, Table } from 'react-bootstrap';
 
 function ManageEmployee() {
+
+    const [employees, setEmployees] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const blue = {
+        backgroundColor: 'lightblue',
+        // color: 'white',
+    }
+
+    const white = {
+        backgroundColor: 'white',
+        // color: 'white',
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/employees', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`
+            }
+        })
+        .then(res => {
+            setEmployees(res.data);
+            setIsLoading(false);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+
     return (
         <>
-        <div>manage employee</div>
-        <Button variant="primary" onClick={() => window.location = '/admin/dashboard'}>Back to Dashboard</Button>
-        <Button> ADD EMPLOYEE</Button>
-        <Button> DELETE EMPLOYEE</Button>
+        <Navbar/>
+        <div className="container">
+            <h1>Manage Employees</h1>
+            <Table bordered hover>
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Position</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {isLoading ? <tr><td>Loading...</td></tr> : employees.map(employee => (
+                        <tr key={employee._id} style={employee.executiveRole === true ? blue : white}>
+                            <td>{employee.name}</td>
+                            <td>{employee.position}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.contact}</td>
+                            <td>
+                                <Button variant="primary">Edit</Button>
+                                <Button variant="danger">Delete</Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
+
+        
         
         </>
     )
