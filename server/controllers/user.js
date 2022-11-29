7d25f8a9879b4a5514120d8109a426e00550f5c0
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2022-11-30 04:16:50 +0800

    UpdateProfilePages

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,14 +6,14 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const getUserByID = async (id, res) => {
-    try {
-        const user = await User.findById(id);
-        hashedPassword = user.password;
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+  try {
+    const user = await User.findById(id);
+    hashedPassword = user.password;
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const loginUser = async (req, res) => {
   try {
@@ -43,30 +43,36 @@ export const loginUser = async (req, res) => {
   }
 };
 
-
-export const registerUser = async (username, rawpassword, confirmPassword, userType, res, role) => {
-    try{
-        const user = await User.findOne({ username });
-        if(user){
-            res.status(400).json({ message: "Username already exists" });
-            return;
-        }
-        if(rawpassword !== confirmPassword){
-            res.status(400).json({ message: "Passwords don't match" });
-            return;
-        }
-        const hashedPassword = await bcrypt.hash(rawpassword, 12);
-        // if role is true, set userType to 3 (executive)
-        if (role) {
-            userType = 3;
-        }
-        const newUser = new User({ username, password: hashedPassword, userType });
-        await newUser.save();
-        return newUser;
-    } catch (error) {
-        res.status(500).json({ message: "Something went wrong" });
+export const registerUser = async (
+  username,
+  rawpassword,
+  confirmPassword,
+  userType,
+  res,
+  role
+) => {
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      res.status(400).json({ message: "Username already exists" });
+      return;
     }
-}
+    if (rawpassword !== confirmPassword) {
+      res.status(400).json({ message: "Passwords don't match" });
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(rawpassword, 12);
+    // if role is true, set userType to 3 (executive)
+    if (role) {
+      userType = 3;
+    }
+    const newUser = new User({ username, password: hashedPassword, userType });
+    await newUser.save();
+    return newUser;
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 // export const registerUser = async (
 //   username,
@@ -88,28 +94,24 @@ export const registerUser = async (username, rawpassword, confirmPassword, userT
 //       return;
 //     }
 
-
-
 export const deleteUser = async (id, res) => {
-    try{
-        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No user with id: ${id}`);
-        await User.findByIdAndRemove(id);
-        res.json({ message: "User deleted successfully." });
-    }
-    catch(error){
-        res.status(500).json({ message: "Something went wrong" });
-    }
-}
-
-    
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send(`No user with id: ${id}`);
+    await User.findByIdAndRemove(id);
+    res.json({ message: "User deleted successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 //update username and password
 export const updateUser = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username } = req.body;
     const user = await User.findByIdAndUpdate(
       req.params.id,
-      { username, password },
+      { username },
       { new: true }
     );
     res.status(200).json(user);
@@ -118,3 +120,21 @@ export const updateUser = async (req, res) => {
   }
 };
 
+export const changePassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+    const user = await User.findById(req.params.id);
+    const isPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Incorrect password!" });
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+    const updateduser = await User.findByIdAndUpdate(
+      req.params.id,
+      { password: hashedPassword },
+      { new: true }
+    );
+    res.status(200).json(updateduser);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,17 @@
-import express from 'express'; // import express
+import express from "express"; // import express
 
-import { AuthToken, AuthAdmin } from '../middleware/Auth.js';
+import { AuthToken, AuthAdmin } from "../middleware/Auth.js";
 
-import {getUserByID} from '../controllers/user.js'; // import the getUsers and createUser functions from the user controller
+import { getUserByID } from "../controllers/user.js"; // import the getUsers and createUser functions from the user controller
 
 const router = express.Router(); // create a router
 
 // router.get('/', AuthAdmin, getUsers);
 
-router.get('/:id', AuthAdmin, getUserByID);
+router.get("/:id", AuthAdmin, getUserByID);
+
+router.get("/user", AuthToken, getUserByID);
+
+router.get("/ChangePassword", AuthToken, getUserByID);
 
 export default router; // export the router
