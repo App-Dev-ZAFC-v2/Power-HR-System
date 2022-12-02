67af6cbcd314412bfcd32170fcc3dde0b0c8e0c8
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-12-02 12:52:15 +0800

    HRS-114 Updated Job Model

--- server/models/job.js
+++ server/models/job.js
@@ -1,23 +1,34 @@
 import mongoose from "mongoose";
 
 const jobSchema = mongoose.Schema({
-    jobName: String,
-    jobDescription: String,
+    name: String,
+    description: String,
+    level: String,
+    salary: Number,
+    location: String,
+    experience: Number,
+    skills: [String],
+    requirements: {
+        type: [String],
+
+    },
+    specializations: {
+        type: [String],
+        enum: ["Not Specified","Accounting/Finance", 
+        "Admin/HR", "Sales/Marketing", 
+        "Arts/Media/Communications", "Services", 
+        "Education/Training", "Tech Support/IT",
+        "Software Development", "Engineering",
+        "Healthcare", "Legal", "Manufacturing",
+        "Science", "Other"],
+        default: ["Not Specified"]
+    },
     quota: Number,
     dateStart: {
         type: Date,
         default: new Date()
     },
     dateEnd: Date,
-    jobStatus: {
-        type: String,
-        default: "Open"
-    },
-    criteria: {
-        type: String,
-        default: "None"
-    },
-    qualification: String,
 });
 
 const Job = mongoose.model("Job", jobSchema);
