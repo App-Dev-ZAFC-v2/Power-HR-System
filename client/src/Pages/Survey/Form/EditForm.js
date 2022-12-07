c2d51ff14f316240ddc6c405593c701fcb6f2223
IYM <muhammadnaim370@gmail.com>
2022-12-08 05:12:21 +0800

    HRS 31 Update drag ui

--- client/src/Pages/Survey/Form/EditForm.js
+++ client/src/Pages/Survey/Form/EditForm.js
@@ -190,95 +190,99 @@ function EditForm(){
                 {(provided) => (
                     <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                         <div>
-                            <div style={{marginBottom: "15px"}}>
-                                <div style={{width:'100%', marginBottom: '-10px' }}>
-                                    <DragIndicatorIcon style={{transform: "rotate(-90deg)", color:'#DAE0E2'}} fontSize="small"/>
-                                </div>
+                            <div style={{marginTop: "15px"}}>
                                 <Accordion onChange={()=>{handleExpand(i)}} expanded={open[i] || false}>
                                     <AccordionSummary aria-controls="panel1a-content" id="panel1a-header" elevation={1} style={{width:'100%'}}>
                                         { !open[i]? (
-                                            <div style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', paddingTop: '15px', paddingBottom: '15px'}}>
-                                                <Typography variant="subtitle1" style={{marginLeft: '0px'}}>{q.questionText}</Typography>
-                                                {(q.questionImage !=="")?(
-                                                    <div>
-                                                        <img src={q.questionImage} width="400px" height="auto" /><br></br><br></br>
-                                                    </div>
-                                                ): "" }
-
-                                                { q.options.map((op, j)=>(
-                                                    <div key={j}>
-                                                        <div style={{display: 'flex'}}>
-                                                            <FormControlLabel disabled control={<Radio style={{marginRight: '3px', }} />} label={
-                                                                <Typography style={{color: '#555555'}}>
-                                                                    {op.optionText}
-                                                                </Typography>
-                                                            } />
-                                                        </div>
-
+                                            <>
+                                                <div style={{display: "flex", justifyContent: "center", alignItems: "center", paddingRight: "12px"}}>
+                                                    <DragIndicatorIcon style={{ color: '#DAE0E2' }} fontSize="small" />
+                                                </div>
+                                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', paddingTop: '15px', paddingBottom: '15px' }}>
+                                                    <Typography variant="subtitle1" style={{ marginLeft: '0px' }}>{q.questionText}</Typography>
+                                                    {(q.questionImage !== "") ? (
                                                         <div>
-                                                            {(op.optionImage !== "")?(
-                                                                <img src={op.optionImage} width="160px" height="auto" />
-                                                            ): "" }
+                                                            <img src={q.questionImage} width="400px" height="auto" /><br></br><br></br>
                                                         </div>
-                                                    </div>
-                                                ))} 
-                                            </div>
+                                                    ) : ""}
+
+                                                    {q.options.map((op, j) => (
+                                                        <div key={j}>
+                                                            <div style={{ display: 'flex' }}>
+                                                                <FormControlLabel disabled control={<Radio style={{ marginRight: '3px', }} />} label={<Typography style={{ color: '#555555' }}>
+                                                                    {op.optionText}
+                                                                </Typography>} />
+                                                            </div>
+
+                                                            <div>
+                                                                {(op.optionImage !== "") ? (
+                                                                    <img src={op.optionImage} width="160px" height="auto" />
+                                                                ) : ""}
+                                                            </div>
+                                                        </div>
+                                                    ))}
+                                                </div></>
                                         ): ""}
                                     </AccordionSummary>
 
-                                    <AccordionDetails>
-                                        <div style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', marginTop:'-15px'}}>
-                                            <div style={{display:'flex', width: '100%', justifyContent: 'space-between'}}>
-                                                <TextField 
-                                                        fullWidth={true} 
-                                                        placeholder="Question Text" 
-                                                        rowsmax={20}
-                                                        multiline={true}
-                                                        value={q.questionText}
-                                                        variant="filled"
-                                                        sx={{m: 1}}
-                                                    onChange={(e)=>{handleQuestionValue(e.target.value, i)}}
-                                                />
-                                                <FormControl sx={{ m: 1, minWidth: 230 }} >
-                                                    <Select value={q.questionType} displayEmpty inputProps={{ 'aria-label': 'Without label' }} >
-                                                        <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Short Answer"}> <ShortTextIcon sx={{mr: "6px"}}/> Short answer</MenuItem>
-                                                        <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Paragraph"}> <SubjectIcon sx={{mr: "6px"}}/> Paragraph</MenuItem>
-                                                        <Divider sx={{ borderBottomWidth: 2, bgcolor: "black"}}/>
-                                                        <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Multiple Choice"}> <RadioButtonCheckedIcon sx={{mr: "6px"}}/> Multiple choice</MenuItem>
-                                                        <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Checkboxes"}> <CheckBoxIcon sx={{mr: "6px"}}/> Checkboxes</MenuItem>
-                                                        <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Drop-down"}> <ArrowDropDownCircleIcon sx={{mr: "6px"}}/> Drop-down</MenuItem>
-                                                        <Divider sx={{ borderBottomWidth: 2, bgcolor: "black" }}/>
-                                                        <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Linear Scale"}> <LinearScaleIcon sx={{mr: "6px"}}/> Linear scale</MenuItem>
-                                                    </Select>
-                                                </FormControl>
+                                    <AccordionDetails >
+                                        <div style={{display: "flex"}}>
+                                            <div style={{display: "flex", justifyContent: "center", alignItems: "center", paddingRight: "12px"}}>
+                                                    <DragIndicatorIcon style={{ color: '#DAE0E2' }} fontSize="small" />
                                             </div>
+                                            <div style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', marginTop:'-15px', width: '100%'}}>
+                                                <div style={{display:'flex', width: '100%', justifyContent: 'space-between'}}>
+                                                    <TextField 
+                                                            fullWidth={true} 
+                                                            placeholder="Question Text" 
+                                                            rowsmax={20}
+                                                            multiline={true}
+                                                            value={q.questionText}
+                                                            variant="filled"
+                                                            sx={{m: 1}}
+                                                        onChange={(e)=>{handleQuestionValue(e.target.value, i)}}
+                                                    />
+                                                    <FormControl sx={{ m: 1, minWidth: 230 }} >
+                                                        <Select value={q.questionType} displayEmpty inputProps={{ 'aria-label': 'Without label' }} >
+                                                            <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Short Answer"}> <ShortTextIcon sx={{mr: "6px"}}/> Short answer</MenuItem>
+                                                            <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Paragraph"}> <SubjectIcon sx={{mr: "6px"}}/> Paragraph</MenuItem>
+                                                            <Divider sx={{ borderBottomWidth: 2, bgcolor: "black"}}/>
+                                                            <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Multiple Choice"}> <RadioButtonCheckedIcon sx={{mr: "6px"}}/> Multiple choice</MenuItem>
+                                                            <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Checkboxes"}> <CheckBoxIcon sx={{mr: "6px"}}/> Checkboxes</MenuItem>
+                                                            <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Drop-down"}> <ArrowDropDownCircleIcon sx={{mr: "6px"}}/> Drop-down</MenuItem>
+                                                            <Divider sx={{ borderBottomWidth: 2, bgcolor: "black" }}/>
+                                                            <MenuItem sx={{pt:"16.5px", pb:"16.5px" }} value={"Linear Scale"}> <LinearScaleIcon sx={{mr: "6px"}}/> Linear scale</MenuItem>
+                                                        </Select>
+                                                    </FormControl>
+                                                </div>
 
-                                            <div style={{width: '100%'}}>
-                                                {q.options.map((op, j) => (
-                                                    <div key={j}>
-                                                        <div  style={{display:'flex', flexDirection:'row', marginLeft:'-12.5px', justifyContent: 'space-between', paddingTop: '5px', paddingBottom: '5px'}}>
-                                                            <Radio disabled />
-                                                            <TextField 
-                                                                fullWidth={true} 
-                                                                placeholder="Option text" 
-                                                                value={op.optionText}
-                                                                onChange={(e)=>{handleOptionValue(e.target.value, i, j)}}
-                                                            />
-                                                            {(q.options.length > 1)? (
-                                                                <IconButton aria-label="delete" onClick={()=>{removeOption(i, j)}} sx={{ml:"12px"}}>
-                                                                    <CloseIcon sx={{m:"6px"}}/>
-                                                                </IconButton>)
-                                                            : ""}
+                                                <div style={{width: '100%'}}>
+                                                    {q.options.map((op, j) => (
+                                                        <div key={j}>
+                                                            <div  style={{display:'flex', flexDirection:'row', marginLeft:'-12.5px', justifyContent: 'space-between', paddingTop: '5px', paddingBottom: '5px'}}>
+                                                                <Radio disabled />
+                                                                <TextField 
+                                                                    fullWidth={true} 
+                                                                    placeholder="Option text" 
+                                                                    value={op.optionText}
+                                                                    onChange={(e)=>{handleOptionValue(e.target.value, i, j)}}
+                                                                />
+                                                                {(q.options.length > 1)? (
+                                                                    <IconButton aria-label="delete" onClick={()=>{removeOption(i, j)}} sx={{ml:"12px"}}>
+                                                                        <CloseIcon sx={{m:"6px"}}/>
+                                                                    </IconButton>)
+                                                                : ""}
+                                                            </div>
+
+                                                            
                                                         </div>
-
-                                                        
+                                                    ))}
+                                                    <div style={{display:'flex', flexDirection:'row', marginLeft:'-12.5px', paddingTop: '5px', paddingBottom: '5px'}}>
+                                                        <Radio disabled />
+                                                        <Button size="small" onClick={()=>{addOption(i)}} style={{textTransform: 'none', marginLeft:"-5px"}}>
+                                                            Add Option
+                                                        </Button>
                                                     </div>
-                                                ))}
-                                                <div style={{display:'flex', flexDirection:'row', marginLeft:'-12.5px', paddingTop: '5px', paddingBottom: '5px'}}>
-                                                    <Radio disabled />
-                                                    <Button size="small" onClick={()=>{addOption(i)}} style={{textTransform: 'none', marginLeft:"-5px"}}>
-                                                        Add Option
-                                                    </Button>
                                                 </div>
                                             </div>
                                         </div>
