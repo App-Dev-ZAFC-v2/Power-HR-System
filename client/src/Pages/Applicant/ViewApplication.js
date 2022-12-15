c628b460f0dafa8272fb5b5789d1727b49f40966
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2022-12-15 21:44:55 +0800

    HRS-104 Create View Application Pages

---
+++ client/src/Pages/Applicant/ViewApplication.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Navbar from "../../Components/Navbar";
+
+function viewapplication() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>View Application</h1>
+      </div>
+    </>
+  );
+}
+
+export default viewapplication;
