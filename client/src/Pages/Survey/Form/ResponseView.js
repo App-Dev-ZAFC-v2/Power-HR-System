01018ef6a39885e156426aae3d35797889a4e27d
IYM <muhammadnaim370@gmail.com>
2023-01-09 13:34:05 +0800

    Update ResponseView.js

--- client/src/Pages/Survey/Form/ResponseView.js
+++ client/src/Pages/Survey/Form/ResponseView.js
@@ -1,8 +1,26 @@
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { DashboardLayout } from "../../../Components/Executive/Dashboard/dashboard-layout";
 import Response from "../../../Components/Survey/Response/Response";
+import { getFormByID } from "../../../Redux/slices/form";
 
 export default function ResponseView(){
+    const { id } = useParams();
+    const rform = useSelector(state => state.forms.form);
+    const dispatch = useDispatch();
+    const retrieveForm = useCallback(() => {
+        dispatch(getFormByID(id));
+    }, [dispatch]);
+
+    useEffect(() => {
+        retrieveForm();
+    }, [retrieveForm]);
+
     return(
-        <Response/>
+        <DashboardLayout tab="Survey Response">
+            <Response/>
+        </DashboardLayout>
     )
 }
 
