c4a2dd79877baa1f332a16ee7a867ee063a91f99
IYM <muhammadnaim370@gmail.com>
2023-01-13 22:46:55 +0800

    Update Dashboard.js

--- client/src/pages/Admin/Dashboard.js
+++ client/src/pages/Admin/Dashboard.js
@@ -1,13 +1,9 @@
-import React, {useState} from 'react';
 import { DashboardLayout } from '../../Components/Admin/Dashboard/dashboard-layout';
 
 function AdminDashboard() {
     return (
-        <>
-        {/* <Navbar/> */}
         <DashboardLayout tab="Dashboard">
         </DashboardLayout>
-        </>
     )
     }
 
