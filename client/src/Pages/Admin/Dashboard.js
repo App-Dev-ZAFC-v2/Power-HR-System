fb1b5082bb39d7c1fe563fbb89251a486b75d2b2
Naim Jalil <86798292+NaimJalill@users.noreply.github.com>
2023-01-14 22:23:13 +0800

    Add files via upload

---
+++ client/src/Pages/Admin/Dashboard.js
@@ -0,0 +1,10 @@
+import { DashboardLayout } from '../../Components/Admin/Dashboard/dashboard-layout.js';
+
+function AdminDashboard() {
+    return (
+        <DashboardLayout tab="Dashboard">
+        </DashboardLayout>
+    )
+    }
+
+export default AdminDashboard;
